bd1067afd4984f9699d8cae1c480b2aa6003990d
Lawrence <moorelawrence.m@gmail.com>
2024-12-27 11:58:05 +1100

    chg mes

--- MyNewWormhole.js
+++ MyNewWormhole.js
@@ -10,7 +10,8 @@ var _reactNative = require("react-native");
 function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
 function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
 //  <-- MUST DO ON UPDATE!! --> 
-// transpile command: npx babel --presets=@babel/preset-env,@babel/preset-react MyNewWormhole.jsx -o MyNewWormhole.js
+// 1. transpile command: npx babel --presets=@babel/preset-env,@babel/preset-react MyNewWormhole.jsx -o MyNewWormhole.js
+// 2. add, commit, push to main
 
 function CustomButton() {
   return /*#__PURE__*/React.createElement(_reactNative.TouchableOpacity, {
